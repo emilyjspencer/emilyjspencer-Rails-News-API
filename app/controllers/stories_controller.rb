6dbbe4409266be0b935667927fa6ffafeda4bbb3
Emily <emilyspencer89@hotmail.co.uk>
2020-03-30 17:26:05 +0100

    Updates the top method in the Stories Controller so that the max number of stories to be show each page is 20. Adds a Bootstrap pagination control  - provides previous and next page functionality

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,8 @@
 class StoriesController < ApplicationController
     def top
-      @stories = client.topstories(0, 20)
+      @start = (params[:start] || 0).to_i
+      @per_page = (params[:per_page] || 10).to_i
+      @per_page = [@per_page, 20].min 
+      @stories = client.topstories(@start, @per_page)
     end
 end  
